b0a79ef1103e97c4f0273663d82d7779220e32bf
Abhinand Kakkadi <nanduttanvsabhi@gmail.com>
2025-09-27 13:42:27 +0530

    implement start auction function

--- pkg/auction/auction.go
+++ pkg/auction/auction.go
@@ -12,6 +12,56 @@ const (
 	AuctionTimeout = 500 * time.Millisecond
 )
 
+func startAuction(auctionId int, wg *sync.WaitGroup, resultChan chan models.AuctionResult) {
+	defer wg.Done()
+
+	var bidWg sync.WaitGroup
+	bidChan, closeSignal := StartBidding(auctionId, &bidWg)
+
+	// Set auction timeout
+	timer := time.NewTimer(AuctionTimeout)
+	startTime := time.Now()
+	var receivedBids []*models.Bid
+
+	bidCollectorDone := make(chan struct{})
+	go func() {
+		defer close(bidCollectorDone)
+		for {
+			select {
+			case bid := <-bidChan:
+				receivedBids = append(receivedBids, bid)
+			case <-timer.C:
+				return // Timeout reached
+			}
+		}
+	}()
+
+	<-bidCollectorDone
+
+	close(closeSignal) // Stop accepting bids
+	bidWg.Wait()       // Make sure all bidders exit
+
+	duration := time.Since(startTime)
+	var winnerId int
+	var winningBid float64 = -1.0
+
+	for _, bid := range receivedBids {
+		if bid.Amount > winningBid {
+			winningBid = bid.Amount
+			winnerId = bid.BidderId
+		}
+	}
+
+	resultChan <- models.AuctionResult{
+		AuctionId:  auctionId,
+		WinnerId:   winnerId,
+		WinningBid: winningBid,
+		Duration:   duration,
+	}
+}
+
+func StartAuctions() {}
+
 func StartBidding(auctionId int, bidWg *sync.WaitGroup) (chan *models.Bid, chan struct{}) {
 	auctionDetails := GenerateAuctionDetails(auctionId)
 
